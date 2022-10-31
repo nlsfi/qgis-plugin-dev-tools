0254fdbae2cf565ac1b7a1f9ac9b7c06912ecbf8
komima <58747243+komima@users.noreply.github.com>
2022-10-31 14:41:25 +0200

    fix: rewrite vendor imports correctly for similar prefix
    - fix invalid imports that were generated, if a vendored package
      has a name that was a substring of the packaged name, and some
      imports were rewritten multiple times

--- test/test_rewrite_imports.py
+++ test/test_rewrite_imports.py
@@ -35,6 +35,7 @@ def test_plain_import_matching_name_rewritten(tmp_path: Path):
     a = 123; import xyz as alias
     a = 123;    import xyz
     a = 123;\timport xyz
+    a = 123; import xyz; b = 456
     \timport xyz
     """
     )
@@ -49,6 +50,38 @@ def test_plain_import_matching_name_rewritten(tmp_path: Path):
     a = 123; import container.package.xyz as alias
     a = 123;    import container.package.xyz as xyz
     a = 123;\timport container.package.xyz as xyz
+    a = 123; import container.package.xyz as xyz; b = 456
     \timport container.package.xyz as xyz
+    """.splitlines()
+    )
+
+
+def test_plain_import_with_mathing_prefix_not_replaced(tmp_path: Path):
+    file = tmp_path / "mock.py"
+
+    file.write_text(
+        """
+    import xyz_a as alias
+    import xyz_b
+    a = 123; import xyz_c as alias
+    a = 123;    import xyz_d
+    a = 123;\timport xyz_e
+    a = 123; import xyz_f; b = 456
+    \timport xyz_g
+    """
+    )
+
+    rewrite_imports_in_source_file(file, "xyz", "container.package")
+
+    assert (
+        file.read_text().splitlines()
+        == """
+    import xyz_a as alias
+    import xyz_b
+    a = 123; import xyz_c as alias
+    a = 123;    import xyz_d
+    a = 123;\timport xyz_e
+    a = 123; import xyz_f; b = 456
+    \timport xyz_g
     """.splitlines()
     )
